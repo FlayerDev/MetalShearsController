685929102169d5d399f4fd50eacd347a16be1dd4
Elias Chr <flayer.dev.gr@gmail.com>
2025-02-16 19:47:08 +0200

    init commit

---
+++ MetalShearsController/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MetalShearsController.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MetalShearsController/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MetalShearsController.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
