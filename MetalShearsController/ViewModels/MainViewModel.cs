f1a468d0aa7c0c70b118dbbf47d2decc1d946b59
Elias Chr <flayer.dev.gr@gmail.com>
2025-02-19 06:37:43 +0200

    Added status bar

--- MetalShearsController/ViewModels/MainViewModel.cs
+++ MetalShearsController/ViewModels/MainViewModel.cs
@@ -6,4 +6,10 @@ public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
     private string _greeting = "Welcome to Avalonia!";
+
+    [ObservableProperty]
+    private string status = "System Offline"; 
+
+    [ObservableProperty]
+    private string statusColor = "Gray";
 }
