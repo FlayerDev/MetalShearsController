1577352fc15518cfd1784da95eea590b08540f0f
Elias Chr <flayer.dev.gr@gmail.com>
2025-02-19 20:26:51 +0200

    Work on mainview & Fixed a service

--- MetalShearsController/Services/InputService.cs
+++ MetalShearsController/Services/InputService.cs
@@ -5,22 +5,26 @@ public class InputService : IDisposable
 {
     private readonly GpioController _controller;
     private readonly int _pin;
+    private readonly PinChangeEventHandler _pinChangeHandler;
 
-    public event Action OnButtonPressed;
+    public event Action OnButtonPressed = delegate { };
 
     public InputService(int pin)
     {
         _controller = new GpioController();
         _pin = pin;
         _controller.OpenPin(_pin, PinMode.InputPullUp);
-        _controller.RegisterCallbackForPinValueChangedEvent(_pin, PinEventTypes.Falling, (s, e) => OnButtonPressed?.Invoke());
+        
+        // Store delegate reference to properly unregister later
+        _pinChangeHandler = (s, e) => OnButtonPressed?.Invoke();
+        _controller.RegisterCallbackForPinValueChangedEvent(_pin, PinEventTypes.Falling, _pinChangeHandler);
     }
 
     public bool IsPressed() => _controller.Read(_pin) == PinValue.Low;
 
     public void Dispose()
     {
-        _controller.UnregisterCallbackForPinValueChangedEvent(_pin, null);
+        _controller.UnregisterCallbackForPinValueChangedEvent(_pin, _pinChangeHandler);
         _controller.ClosePin(_pin);
         _controller.Dispose();
     }
