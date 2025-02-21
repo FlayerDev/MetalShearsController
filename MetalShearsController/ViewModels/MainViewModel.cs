a1f6e208a58d53d082a94a046a600d0e048a1e2c
Elias Chr <flayer.dev.gr@gmail.com>
2025-02-21 13:05:19 +0200

    Added buttons and icons

--- MetalShearsController/ViewModels/MainViewModel.cs
+++ MetalShearsController/ViewModels/MainViewModel.cs
@@ -1,4 +1,9 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Avalonia.Interactivity;
+using CommunityToolkit.Mvvm.ComponentModel;
+using ReactiveUI;
+using System;
+using System.Diagnostics;
+using System.Windows.Input;
 
 namespace MetalShearsController.ViewModels;
 
@@ -13,4 +18,19 @@ public partial class MainViewModel : ViewModelBase
     
     [ObservableProperty]
     private string requestedTerminalPosition = "0000.0mm";
+    private int requestedTerminalPositionUnit = 0;
+    public ICommand IncrementRequestedTeminalPositionCommand { get; }
+
+    public MainViewModel()
+    {
+        IncrementRequestedTeminalPositionCommand = ReactiveCommand.Create(IncrementRequestedTeminalPosition);
+
+    }
+
+    private void IncrementRequestedTeminalPosition ()
+    {
+        Debug.Print("!!");
+        //requestedTerminalPositionUnit += int.Parse(value);
+        //RequestedTerminalPosition = (requestedTerminalPositionUnit).ToString() + " mm";
+    }
 }
